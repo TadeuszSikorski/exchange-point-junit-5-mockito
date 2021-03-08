fa153253ede0249ae4030cf97eb0fbe2204b5322
TadeuszSikorski <sikorskitadeusz@yahoo.com>
2021-03-08 14:34:03 +0100

    TDD: GREEN phase

---
+++ README.md
@@ -0,0 +1,3 @@
+# Currency Exchange Point
+
+A simple currency exchange app to learn about JUnit 5 and Mockito.
--- src/test/java/pl/sikorski/tadeusz/CurrencyExchangeTest.java
+++ src/test/java/pl/sikorski/tadeusz/CurrencyExchangeTest.java
@@ -19,81 +19,80 @@ public class CurrencyExchangeTest {
     @Mock
     ExchangeRatesProvider exchangeRatesProvider;
 
-    @Mock
-    CurrencyExchange currencyExchange;
-
     @ParameterizedTest
     @ValueSource(strings = {"0.00", "-0.01", "-1.00"})
     void spreadEqualToOrLessThanZero(String spread) {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new CurrencyExchange(new BigDecimal(spread), exchangeRatesProvider);
-        });
-        
-        fail(exception.getMessage());
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, 
+            () -> {
+                CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal(spread), exchangeRatesProvider);
+            });
+
+        assertEquals("Spread must be greater than zero and less than one.", exception.getMessage());
     }
     
     @ParameterizedTest
     @ValueSource(strings = {"1.00","5.00"})
     void spreadEqualToOrGreaterThanOne(String spread) {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            new CurrencyExchange(new BigDecimal(spread), exchangeRatesProvider);
-        });
-        
-        fail(exception.getMessage());
-    }
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, 
+            () -> {
+                CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal(spread), exchangeRatesProvider);
+            });
 
-    @Test
-    void exchangeRateEqualToZero() {
-        IncorrectExchangeRatesException exception = assertThrows(IncorrectExchangeRatesException.class, () -> {
-            when(exchangeRatesProvider.getExchangeRate(any(CurrencyCode.class), any(CurrencyCode.class))).thenReturn(new BigDecimal("0.0"));
-        });
-        
-        fail(exception.getMessage());
-    }
-
-    @Test
-    void exchangeRateLessThanZero() {
-        IncorrectExchangeRatesException exception = assertThrows(IncorrectExchangeRatesException.class, () -> {
-            when(exchangeRatesProvider.getExchangeRate(any(CurrencyCode.class), any(CurrencyCode.class))).thenReturn(new BigDecimal("-1.0"));
-        });
-        
-        fail(exception.getMessage());
+        assertEquals("Spread must be greater than zero and less than one.", exception.getMessage());
     }
 
     @Test
     void bidPriceForSpecifiedZeroAmountOfMoneyEURtoPLN() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            currencyExchange.getBidPrice(new BigDecimal("0.00"), CurrencyCode.EUR, CurrencyCode.PLN);
-        });
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, 
+            () -> {
+                CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
+
+                currencyExchange.getBidPrice(new BigDecimal("0.00"), CurrencyCode.EUR, CurrencyCode.PLN);
+            });
         
-        fail(exception.getMessage());
+        assertEquals("Amount of money must be greater than zero.", exception.getMessage());
     }
 
     @Test
     void askPriceForSpecifiedZeroAmountOfMoneyEURtoPLN() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            currencyExchange.getAskPrice(new BigDecimal("0.00"), CurrencyCode.EUR, CurrencyCode.PLN);
-        });
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, 
+            () -> {
+                CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
+
+                currencyExchange.getAskPrice(new BigDecimal("0.00"), CurrencyCode.EUR, CurrencyCode.PLN);
+            });
         
-        fail(exception.getMessage());
+        assertEquals("Amount of money must be greater than zero.", exception.getMessage());
     }
 
     @Test
     void bidPriceForSpecifiedLessThanZeroAmountOfMoneyEURtoPLN() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            currencyExchange.getBidPrice(new BigDecimal("-1.00"), CurrencyCode.EUR, CurrencyCode.PLN);
-        });
-        
-        fail(exception.getMessage());
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, 
+            () -> {
+                CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
+
+                currencyExchange.getBidPrice(new BigDecimal("-1.00"), CurrencyCode.EUR, CurrencyCode.PLN);
+            });
+
+        assertEquals("Amount of money must be greater than zero.", exception.getMessage());
     }
 
     @Test
     void askPriceForSpecifiedLessThanZeroAmountOfMoneyEURtoPLN() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            currencyExchange.getAskPrice(new BigDecimal("-1.00"), CurrencyCode.EUR, CurrencyCode.PLN);
-        });
-        
-        fail(exception.getMessage());
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class, 
+            () -> {
+                CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
+
+                currencyExchange.getAskPrice(new BigDecimal("-1.00"), CurrencyCode.EUR, CurrencyCode.PLN);
+            });
+
+        assertEquals("Amount of money must be greater than zero.", exception.getMessage());
     }
 
     @Test
@@ -101,7 +100,7 @@ public class CurrencyExchangeTest {
         when(exchangeRatesProvider.getExchangeRate(CurrencyCode.EUR, CurrencyCode.PLN)).thenReturn(new BigDecimal("4.5540"));
         CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
 
-        assertEquals(new BigDecimal("454.40"), currencyExchange.getBidPrice(new BigDecimal("100.00"), CurrencyCode.EUR, CurrencyCode.PLN));
+        assertEquals(new BigDecimal("454.4000"), currencyExchange.getBidPrice(new BigDecimal("100.00"), CurrencyCode.EUR, CurrencyCode.PLN));
     }
 
     @Test
@@ -109,7 +108,7 @@ public class CurrencyExchangeTest {
         when(exchangeRatesProvider.getExchangeRate(CurrencyCode.EUR, CurrencyCode.PLN)).thenReturn(new BigDecimal("4.5540"));
         CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
 
-        assertEquals(new BigDecimal("453.40"), currencyExchange.getAskPrice(new BigDecimal("100.00"), CurrencyCode.EUR, CurrencyCode.PLN));
+        assertEquals(new BigDecimal("456.4000"), currencyExchange.getAskPrice(new BigDecimal("100.00"), CurrencyCode.EUR, CurrencyCode.PLN));
     }
 
     @Test
@@ -117,7 +116,7 @@ public class CurrencyExchangeTest {
         when(exchangeRatesProvider.getExchangeRate(CurrencyCode.PLN, CurrencyCode.EUR)).thenReturn(new BigDecimal("0.2196"));
         CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
 
-        assertEquals(new BigDecimal("20.96"), currencyExchange.getBidPrice(new BigDecimal("100.00"), CurrencyCode.PLN, CurrencyCode.EUR));
+        assertEquals(new BigDecimal("20.9600"), currencyExchange.getBidPrice(new BigDecimal("100.00"), CurrencyCode.PLN, CurrencyCode.EUR));
     }
 
     @Test
@@ -125,6 +124,6 @@ public class CurrencyExchangeTest {
         when(exchangeRatesProvider.getExchangeRate(CurrencyCode.PLN, CurrencyCode.EUR)).thenReturn(new BigDecimal("0.2196"));
         CurrencyExchange currencyExchange = new CurrencyExchange(new BigDecimal("0.0200"), exchangeRatesProvider);
 
-        assertEquals(new BigDecimal("22.96"), currencyExchange.getAskPrice(new BigDecimal("100.00"), CurrencyCode.PLN, CurrencyCode.EUR));
+        assertEquals(new BigDecimal("22.9600"), currencyExchange.getAskPrice(new BigDecimal("100.00"), CurrencyCode.PLN, CurrencyCode.EUR));
     }
 }
